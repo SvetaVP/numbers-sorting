68881d71965979d341bb879abd44539f8fdff191
Sveta <qwe7vbn12@gmail.com>
2020-11-18 01:06:20 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 "use strict";
 
-const number = [9, 6, 1, 2, 4, 10, 5, 7, 3, 8, 0];
+const numbers = [9, 6, 1, 2, 4, 10, 15, 7, 3, 8, 0];
+const sortedNumbers = console.log(sort(numbers));
+
+function sort(numbers) {
+  const items = [];
+  let index = 0;
+
+  for (const number of numbers) {
+    if (number[index] === index) {
+      index++;
+      console.log("+");
+    } else {
+      items[number] = number;
+    }
+  }
+
+  return items;
+}
+
+// I'm still thinking about it.
+// I know that this option is not correct and there are many vulnerabilities in it.
+// This is because I am not allowed to pry
+// :-D
