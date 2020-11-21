97b6d90c8494b701694f7f824f760b538e3be8bf
Sveta <qwe7vbn12@gmail.com>
2020-11-21 23:18:37 +0200

    added a function to determine if the array is sorted or not

--- js/script.js
+++ js/script.js
@@ -1,25 +1,22 @@
 "use strict";
 
 const numbers = [9, 6, 1, 2, 4, 10, 15, 7, 3, 8, 0];
-const sortedNumbers = console.log(sort(numbers));
+console.log(isSorted(numbers));
 
-function sort(numbers) {
-  const items = [];
-  let index = 0;
-
-  for (const number of numbers) {
-    if (number[index] === index) {
-      index++;
-      console.log("+");
-    } else {
-      items[number] = number;
+function isSorted(numbers) {
+  for (let firstIndex = 0; firstIndex <= numbers.length; firstIndex++) {
+    for (
+      let secondIndex = firstIndex + 1;
+      secondIndex <= numbers.length - 1;
+      secondIndex++
+    ) {
+      if (numbers[firstIndex] < numbers[secondIndex]) {
+        continue;
+      } else {
+        return false;
+      }
     }
+
+    return true;
   }
-
-  return items;
 }
-
-// I'm still thinking about it.
-// I know that this option is not correct and there are many vulnerabilities in it.
-// This is because I am not allowed to pry
-// :-D
