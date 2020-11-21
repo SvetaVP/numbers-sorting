458155c4523fa1cb0afd305693fdb94498e03d5f
Sveta <qwe7vbn12@gmail.com>
2020-11-22 00:30:12 +0200

    added sort function

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 "use strict";
 
-const numbers = [9, 6, 1, 2, 4, 10, 15, 7, 3, 8, 0];
-console.log(isSorted(numbers));
+const numbers = [9, 6, 1, 2, 4, 10, -3, 15, 7, 3, 8, 0];
+console.log(sort(numbers));
 
-function isSorted(numbers) {
+function sort(numbers) {
   for (let firstIndex = 0; firstIndex <= numbers.length; firstIndex++) {
     for (
       let secondIndex = firstIndex + 1;
       secondIndex <= numbers.length - 1;
       secondIndex++
     ) {
-      if (numbers[firstIndex] < numbers[secondIndex]) {
-        continue;
-      } else {
-        return false;
+      if (numbers[firstIndex] > numbers[secondIndex]) {
+        [numbers[firstIndex], numbers[secondIndex]] = [
+          numbers[secondIndex],
+          numbers[firstIndex],
+        ];
       }
     }
-
-    return true;
   }
+
+  return numbers;
 }
+
+// I did not understand how to use 2 functions in this task
